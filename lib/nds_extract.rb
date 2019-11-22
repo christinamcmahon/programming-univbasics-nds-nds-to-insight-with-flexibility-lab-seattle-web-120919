21aa7f3a61c34a362f10f40cc0bdd0ff79bbfd49
Christina McMahon <christina.mcmahon1@gmail.com>
2019-11-22 21:58:26 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -47,7 +47,11 @@ def movies_with_director_key(name, movies_collection)
   # :director_name key. This addition can be done by using the provided
   # movie_with_director_name method
   aoh = []
-  
+  index = 0 
+  while index < movies_collection.length do 
+    aoh.push(movie_with_director_name(movies_collection[index]))
+  end
+  aoh
 end
 
 
